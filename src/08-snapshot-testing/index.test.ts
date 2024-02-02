fa9cb9d084f95638d746000d09f8741ed366ef7e
Mmishaaa <kkochin2528@gmail.com>
2024-02-02 12:56:40 +0300

    feat: add snapshot-testing tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,40 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = {
+      value: 10,
+      next: {
+        value: 11,
+        next: {
+          value: 12,
+          next: {
+            value: 13,
+            next: { value: 100, next: { value: null, next: null } },
+          },
+        },
+      },
+    };
+    expect(values).toStrictEqual(generateLinkedList([10, 11, 12, 13, 100]));
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = {
+      value: 10,
+      next: {
+        value: 11,
+        next: {
+          value: 12,
+          next: {
+            value: 13,
+            next: { value: 100, next: { value: null, next: null } },
+          },
+        },
+      },
+    };
+    expect(values).toMatchSnapshot(generateLinkedList([10, 11, 12, 13, 100]));
   });
 });
